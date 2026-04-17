3ed71d9ee82413af2e0792708e8e90d1e4d854f1
OwenKalow <kalow012@umn.edu>
2026-04-17 14:21:58 -0500

    Add files via upload

---
+++ Color_Sensor/color_sensor_lib.c
@@ -0,0 +1,82 @@
+/* 
+ * File:   color_sensor_lib.c
+ * Author: owenk
+ *
+ * Created on April 14, 2026, 5:33 PM
+ */
+
+#include "xc.h"
+#include "color_sensor_lib.h"
+
+void Color_Init(void) {
+    //BP configuration
+    CLKDIVbits.RCDIV = 0;  //Set RCDIV=1:1 (default 2:1) 32MHz or FCY/2=16M
+    AD1PCFG = 0x9fff;            //sets all pins to digital I/O
+    TRISA = 0b0000000000011111;  //set port A to inputs, 
+    TRISB = 0b0000000000000011;  //and port B to outputs
+    LATA = 0xffff;               //Set all of port A to HIGH
+    LATB = 0xffff;               //and all of port B to HIGH
+    
+    //I2C1 initialization
+    I2C1CON = 0; //disable and reset control register
+    I2C1BRG = 157; //FSCL = ~100kHz
+    _MI2C1IF = 0; //clear interrupt flag
+    I2C1CONbits.I2CEN = 1; //enable
+    
+    Delayms(5);
+    
+    //Initialization calls
+    Color_Cmd(0x80, 0x01); //device enable
+    Delayms(5);
+    Color_Cmd(0x80, 0x12); //RBGC enable   
+}
+
+void Color_Cmd(char command, char data) {
+    I2C1CONbits.SEN = 1;
+    while(_SEN); //wait for startup to complete
+    
+    PrintFrame(address<<1); //sensor address with R/nW = 0
+    PrintFrame(command); 
+    PrintFrame(data);
+    
+    I2C1CONbits.PEN = 1;
+    while (_PEN); //wait for shutdown to complete
+}
+
+int Color_Read(void) {
+    int colorVal = 0;
+    
+    I2C1CONbits.SEN = 1;
+    while(_SEN); //wait for startup to complete
+
+    PrintFrame(address<<1 | 1); //sensor address with R/nW = 1
+    colorVal = GetByte(); //add lower byte
+    colorVal += GetByte()<<8; //add upper byte
+    
+    I2C1CONbits.PEN = 1;
+    while (_PEN); //wait for shutdown to complete
+    
+    return(colorVal);
+}
+
+void PrintFrame(char byte) {
+    _MI2C1IF = 0;
+    I2C1TRN = byte;
+    while ((!_MI2C1IF) & _TRSTAT & (!_ACKSTAT)); //waits for transmission to finish 
+}
+
+char GetByte(void){
+    _RCEN = 1; //activate receive mode
+    while(!_RBF); //wait for receive to complete
+    _ACKEN = 1; //send acknowledge bit
+    while(_ACKEN);
+    return(I2C1RCV);
+}
+
+void Delayms(int time) {
+    for(int i=0;i<time;i++) {
+        for(int j=0;j<2000;j++){
+            //loop
+        }
+    }
+}
---
+++ Color_Sensor/color_sensor_lib.h
@@ -0,0 +1,30 @@
+/* 
+ * File:   color_sensor_lib.h
+ * Author: owenk
+ *
+ * Created on April 14, 2026, 5:33 PM
+ */
+
+#ifndef COLOR_SENSOR_LIB_H
+#define	COLOR_SENSOR_LIB_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+    
+    #define address 0b0101001
+
+    void PrintFrame(char byte);
+    char GetByte(void);
+    void Color_Init(void);
+    void Color_Cmd(char command, char data);
+    int Color_Read(void);
+    void Delayms(int time);
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* COLOR_SENSOR_LIB_H */
+
